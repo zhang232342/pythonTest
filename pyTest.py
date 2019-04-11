990ad8e7793c989f55ab434ef11301ea967d6d3f
candyz <648245419@qq.com>
2019-04-11 10:07:23 +0800

    完成ExcelToWord

---
+++ pyTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @TIME     :2019/4/10 15:41
+# @Author   :CandyZ
+# @File     :pyTest.py
+import urllib.request
+
+url = 'http://www.baidu.com'
+page = urllib.request.urlopen(url)
+htmlCode = page.read()
+pageFile = open('D:/pageFile.txt','wb+')
+pageFile.write(htmlCode)
+pageFile.close()#关闭
+print('已写入')

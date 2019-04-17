18cdc78daa32e8cbe9a18257b9dcb52a50ca8620
candyz <648245419@qq.com>
2019-04-17 10:51:35 +0800

    自动提醒

--- autoMail.py
+++ autoMail.py
@@ -5,20 +5,45 @@
 # @File     :autoMail.py
 
 import smtplib
+import xlrd
+import random
+from xlutils.copy import copy
 from email.mime.text import MIMEText
 from email.header import Header
 
 my_sender = 'zhang342342@sina.com'  # 发件人邮箱
 my_pass = 'zhangpeng321'  # 发件人邮箱密码
-my_user = '648245419@qq.com'
-subject = 'python测试邮件'
-
+my_user = 'zhang342342@icloud.com'
+subject = '请坚持健身'
+textRemind = ''
+# 设置文件名和路径
+fname = 'd:/autoEmail/记录表.xls'
+# 打开文件
+filename = xlrd.open_workbook(fname)
+# 获取当前文档的表(得到的是sheet的个数，一个整数）
+sheets = filename.nsheets
+sheet = filename.sheets()[0]  # 通过sheet索引获得sheet对象
+# print sheet
+# 获取行数
+nrows = sheet.nrows
+# 获取列数
+ncols = sheet.ncols
+# 获取第一行,第一列数据数据
+cell_value = sheet.cell_value(0, 0)
+num = int(cell_value+1)
+wb = copy(filename)
+ws = wb.get_sheet(0)
+ws.write(0, 0, num)
+wb.save('d:/autoEmail/记录表.xls')
+randomValue = random.randint(0, 99)
+cell_values = sheet.cell_value(randomValue, 1)
+sendMail = '今天是坚持健身的第'+str(num)+'天,请继续坚持下去。'+'\n\n\n'+'语录：'+cell_values
 
 def mail():
     ret = True
     try:
         msg = MIMEText(
-            '来自机器人发送的测试邮件',
+            sendMail,
             'plain',
             'utf-8')  # 中文需参数‘utf-8'，单字节字符不需要
         msg['Subject'] = Header(subject, 'utf-8')  # 新浪邮箱设置标题 使用新浪邮箱必写
